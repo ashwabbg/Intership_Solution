5d0f32ec444da007cb0eb17b801d532f69229ef2
M0N0G0N <thibauddandoy3d@gmail.com>
2023-12-14 11:02:31 +0100

    First model

---
+++ src/main/java/com/example/intership_solution/controller/InternshipController.java
@@ -0,0 +1,27 @@
+package com.example.intership_solution.controller;
+
+import com.example.intership_solution.model.Internship;
+import com.example.intership_solution.service.InternshipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/internship")
+public class InternshipController {
+
+    @Autowired
+    private InternshipService internshipService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Internship internship) {
+        internshipService.saveInternship(internship);
+        return "New internship is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Internship> getAllInternships() {
+        return internshipService.getAllInternships();
+    }
+}
---
+++ src/main/java/com/example/intership_solution/model/Internship.java
@@ -0,0 +1,42 @@
+package com.example.intership_solution.model;
+
+import jakarta.persistence.*;
+
+import java.time.DateTimeException;
+import java.util.Date;
+
+@Entity
+public class Internship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int internship_id;
+
+    private String title;
+
+    private Date date;
+
+    public int getInternship_id() {
+        return internship_id;
+    }
+
+    public void setInternship_id(int internship_id) {
+        this.internship_id = internship_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
