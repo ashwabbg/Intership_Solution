0fc9d8f6e89a8ab0586082f3dba9da77030e2f60
Billy-Gagnaire Baptiste <baptiste.billygagnaire2@gmail.com>
2023-12-14 09:11:01 +0100

    First Commit

---
+++ src/main/java/com/example/intership_solution/IntershipSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.intership_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershipSolutionApplication.class, args);
+	}
+
+}
